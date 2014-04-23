7ef4a6d80b871b3b04773cc64a76f71518e66084
Andrew Yang <evil_eve_live@outlook.com>
2014-04-23 22:54:42 +0800

    Initial version.

---
+++ ProjectEuler.Core/PEMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MathNet.Numerics;
+
+namespace ProjectEuler.Core
+{
+    
+    public class PEMath
+    {
+        public bool IsPrime(Int64 value) {
+            throw new NotImplementedException();
+        }
+    }
+}
